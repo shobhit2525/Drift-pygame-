d9963c6da3baba6b4ce4092a06ba76d1152a89c6
Shobhit Rana <shobhit4136@gmail.com>
2024-12-06 14:33:06 +0530

    movement of car

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 import math
-from utils import scale_image
+from utils import scale_image, blit_rotate_center
 
 
 GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
@@ -10,8 +10,8 @@ TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
 TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
 FINISH = pygame.image.load("imgs/finish.png")
 
-RED_CAR = pygame.image.load("imgs/red-car.png")
-GREEN_CAR = pygame.image.load("imgs/green-car.png")
+RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"),0.55)
+GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"),0.55)
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -19,21 +19,85 @@ pygame.display.set_caption("DRIFT!")
 
 FPS = 60
 
+class AbstractCar:
+    IMG = RED_CAR
+    def __init__(self, max_vel, rotation_vel):
+        self.img = self.IMG
+        self.max_vel = max_vel
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+        self.x, self.y = self.START_POS
+        self.acceleration = 0.1
+    
+    def rotate(self, Left=False, right=False):
+        if Left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+    
+    def draw(self, win):
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+    
+    def move_forward(self):
+        self.vel = min(self.vel + self.acceleration, self.max_vel)
+        self.move()
+    
+    def move(self):
+        radians = math.radians(self.angle)
+        vertical = math.cos(radians) * self.vel
+        horizontal = math.sin(radians) * self.vel
+        
+        self.y -= vertical
+        self.x -= horizontal
+    
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
+        
+
+class PlayerCar(AbstractCar):
+    IMG = RED_CAR   
+    START_POS = (180, 200) 
+
+def draw(win, images, player_car):
+    for img, pos in images:
+        win.blit(img, pos)
+        
+    player_car.draw(win)
+    pygame.display.update()
+
 run = True
 clock = pygame.time.Clock()
+images = [(GRASS, (0,0)), (TRACK, (0,0))]
+player_car = PlayerCar(4,4)
 
 while run:
     clock.tick(FPS)
+    draw(WIN, images, player_car)
     
-    WIN.blit(GRASS,(0,0))
-    WIN.blit(TRACK,(0,0))
-    WIN.blit(FINISH,(0,0))
-    
-    pygame.display.update()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             break
+    
+    keys = pygame.key.get_pressed()
+    
+    moved = False
+    
+    if keys[pygame.K_a]:
+        player_car.rotate(Left = True)
+    if keys[pygame.K_d]:
+        player_car.rotate(right = True)
+    if keys[pygame.K_w]:
+        moved = True
+        player_car.move_forward()
+    
+    if not moved:
+        player_car.reduce_speed()
+
+        
+        
 
 pygame.quit()
